bd65112a4bc90cebfa81b79aab7fffb4c52468a8
MQabaja <154758224+MQabaja@users.noreply.github.com>
2025-05-17 21:05:08 +0300

    Add files via upload

---
+++ app2/index.js
@@ -0,0 +1,51 @@
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function getRandomCard() {
+  let rand = Math.floor(Math.random() * 13) + 1
+  if (rand > 10) return 10
+  else if (rand === 1) return 11
+  else return rand
+}
+
+function startGame() {
+  isAlive = true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+  cards = [firstCard, secondCard]
+  sum = firstCard + secondCard
+  renderGame()
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: " + cards.join(" ")
+  sumEl.textContent = "Sum: " + sum
+
+  if (sum < 21) {
+    message = "Do you want to draw a new card?"
+  } else if (sum === 21) {
+    message = "You've got Blackjack!"
+    hasBlackJack = true
+  } else {
+    message = "You're out of the game!"
+    isAlive = false
+  }
+
+  messageEl.textContent = message
+}
+
+function newCard() {
+  if (isAlive && !hasBlackJack) {
+    let card = getRandomCard()
+    cards.push(card)
+    sum += card
+    renderGame()
+  }
+}
